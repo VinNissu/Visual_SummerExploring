308b68c2d961aae67b40de136eef0a3778390eb5
VinnyAlmeidaBR <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-09-14 11:50:49 -0300

    Update

--- js/custom.js
+++ js/custom.js
@@ -36,7 +36,29 @@ const getEstados = () => {
 
     for (const index in json) {
       // console.log(json [index].nome)
-      options += '<option>' + json[index].nome + '</option>'
+      options += `<option value = ${json[index].sigla}>${json[index].nome}</option>`
+    }
+
+
+    select.innerHTML = options
+  })
+}
+
+
+// Preencha o select de cidades de acordo com o UF selecionado 
+//A função recebe um parâmetro com o ID da UF
+
+const getCidadesByUf = (uf) => {
+  let api = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+  let select = document.getElementById('cidade')
+
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione </option >'
+
+
+    for (const index in json) {
+
+      options += `<option value = ${json[index].nome}>${json[index].nome}</option>`
     }
 
 
@@ -67,29 +89,99 @@ for (let index = 0; index < xuxu.length; index++) {
 //     aviso('Hello World, este é o planeta Terra???')
 // });
 
-getEstados()
 
+
+/* ------------------------------------------------*/
+
+/* const getEstados = () => {
+  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+  let select = document.getElementById('estado')
+
+  //Lê a API atravês do Fetch(), 1º then captura os dados, 2º then trata os dados
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione </option >'
+
+    //console.log(json)
+
+    for (const index in json) {
+      // console.log(json [index].nome)
+      options += '<option>' + json[index].nome + '</option>'
+    }
+
+
+    select.innerHTML = options
+  })
+}
+ */
+
+getEstados()
+/* -------------------------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------*/
+
+// const mostraIdade = () => {
+//   let span = document.getElementById('txt-idade')
+//   let campoIdade = document.getElementById('idade')
+//   span.innerText = campoIdade.value
+// }
 
 mostraIdade()
 document.getElementById('idade').addEventListener('change', mostraIdade)
 
-
-
 mostraData()
+/* -------------------------------------------------------------------------------------------- */
 
 
 
 
+
+
+
+
+
+
+
+/* ------------------------------------------------*/
 // inicializa animações do AOS 
 AOS.init();
 
+/* -------------------------------------------------------------------------------------------- */
 
 
 
+
+
+
+
+
+
+
+/* ------------------------------------------------*/
 ////Impede o envio do formulário quando os campos estão inválidos\\\\
 (function () {
   'use strict'
+  /* -------------------------------------------------------------------------------------------- */
 
+
+
+
+
+
+  /* ------------------------------------------------*/
 
   ////variável  captura as tags <form que contém a classe "needs-validation"\\\\\
   var forms = document.querySelectorAll('.needs-validation')
@@ -112,4 +204,28 @@ AOS.init();
     })
 })()
 
-/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\FIM//////////////////////////////// */
+
+/* -------------------------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+/* ------------------------------------------------*/
+// \\
+
+// const getCidadesByUf = (uf) => { 
+//   alert(uf)
+// }
+
+document.getElementById('estado').addEventListener('change', function () {
+  getCidadesByUf(this.value)
+})
+
+// 'this' represeta     objeto  'estado'
+// \\
+/* -------------------------------------------------------------------------------------------- */
